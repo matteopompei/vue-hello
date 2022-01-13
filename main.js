8562ae01e30bc9fcbf6606a7508c1195355df640
Matteo Pompei <datxizzan@gmail.com>
2022-01-13 15:12:05 +0100

    Immagine aggiunta

--- main.js
+++ main.js
@@ -2,5 +2,6 @@ let app = new Vue({
   el: "#app",
   data: {
     text: "Hello world!",
+    image: "https://static.fanpage.it/wp-content/uploads/sites/6/2021/05/knuckles-meme-1200x1200.jpg"
   }
 });
